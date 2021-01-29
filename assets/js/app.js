7e97532ad6db2670b57a88ee686fe4d4fc5e01f5
zekariyas alemu <zekialemu3@gmail.com>
2021-01-29 10:07:42 +0300

    calc

---
+++ assets/js/app.js
@@ -0,0 +1,71 @@
+// Define UI Variables 
+const taskInput = document.querySelector('#task'); //the task input text field
+const form = document.querySelector('#task-form'); //The form at the top
+const filter = document.querySelector('#filter'); //the task filter text field
+const taskList = document.querySelector('.collection'); //The UL
+const clearBtn = document.querySelector('.clear-tasks'); //the all task clear button
+
+const reloadIcon = document.querySelector('.fa'); //the reload button at the top navigation 
+form.addEventListener('submit', addNewTask);
+clearBtn.addEventListener('click', clearAllTasks);
+filter.addEventListener('keyup', filterTasks);
+taskList.addEventListener('click', removeTask); 
+reloadIcon.addEventListener('click', reloadPage);
+
+
+function addNewTask(e) {
+
+    e.preventDefault(); 
+
+
+    // Check empty entry
+    if (taskInput.value === '') {
+        taskInput.style.borderColor = "red";
+
+        return;
+    }
+
+    // Create an li element when the user adds a task 
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new element for the link 
+    const link = document.createElement('a');
+    // Add class and the x marker for a 
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append link to li
+    li.appendChild(link);
+    // Append to UL 
+    taskList.appendChild(li);
+} 
+function clearAllTasks() {
+
+    //This is the first way 
+    // taskList.innerHTML = '';
+
+    //  Second Wy 
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+
+}
+
+function filterTasks(e) {
+
+   
+
+}
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are You Sure about that ?')) {
+            e.target.parentElement.parentElement.remove();
+
+        }
+
+    }
+}
+
+function reloadPage() {
+    location.reload();
+}
